93ece5c7a119e4143bccc3b257656f0924b1a954
Irelandoracle1 <nairajnr1@gmail.com>
2024-04-26 19:35:05 +0000

    made code keep asking for datat if data is invalid

--- run.py
+++ run.py
@@ -16,15 +16,23 @@ def get_sales_data():
     """
     get sales data
     """
-    print("please enter sales data from last market.")
-    print("Data should be six numbers , seperated by commas")
-    print("Examples: 10,20,30,30,50,60\n")
-
-    data_str = input("Enter your data here: ")
+    while True:
+        print("please enter sales data from last market.")
+        print("Data should be six numbers , seperated by commas")
+        print("Examples: 10,20,30,30,50,60\n")
+        
+        data_str = input("Enter your data here: ")
     
+        sales_data = data_str.split(",")
+        
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data) 
+        if validate_data(sales_data):
+            print("Data is Valid")
+            break
+
+
+   
+    
 
 def validate_data(values):
     """
@@ -32,12 +40,21 @@ def validate_data(values):
     and except
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"invalid data: {e}, please try again.\n")        
+        print(f"invalid data: {e}, please try again.\n")
+        return False        
+
+    return True
+
+
+
+
+data = get_sales_data() 
 
 
 
@@ -47,7 +64,13 @@ def validate_data(values):
 
 
 
-get_sales_data() 
+
+
+
+
+
+
+
 
 
 
