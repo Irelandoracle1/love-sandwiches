56d7d2ee8ae7ce1aaabb7dd81ceb92cb51a98c0e
Irelandoracle1 <nairajnr1@gmail.com>
2024-04-27 12:19:56 +0000

    refactor update worksheet functions into 1

--- run.py
+++ run.py
@@ -54,25 +54,15 @@ def validate_data(values):
     return True
 
 
-
-def update_sales_worksheet(data):
+def update_worksheet(data, worksheet):
     """
-    update sales worksheet and add new row with list of data provided
+    receive list of integers to be inserted into a worksheet
+    update relivant worksheet with data provided
     """
-    print("updating sales worksheet....\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("sales worksheet updated succesfully\n")
-
-
-def update_surplus_worksheet(data):
-    """
-    update surplus worksheet and add new row with list of data provided
-    """
-    print("updating surplus worksheet....\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("surplus worksheet updated succesfully\n")
+    print(f"updating {worksheet} worksheet...\n") 
+    worksheet_to_update = SHEET.worksheet(worksheet)   
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully")
 
 def caculate_surplus_data(sales_row):
     """
@@ -106,9 +96,9 @@ def main():
     """
     data = get_sales_data() 
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = caculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 
 
